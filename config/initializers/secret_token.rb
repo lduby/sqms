8759cdc5d554c75baafd4e2bea452456ce99a545
Laetitia Duby <laetitia.duby@gmail.com>
2011-10-10 12:37:07 +0200

    Base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sqms::Application.config.secret_token = 'f8e6e0200a58df2b4286bc26488c57a459effce5b2c109bd02260c759006e4f6df4d2470ae4c7599d0b0bfcec0004f0c8eb052ff3485945b3b45f6eb2da93669'
